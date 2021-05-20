b56783f7d7edd7e63084e767f60bc4ab3c85382f
DanielaCorbetta <81688280+DanielaCorbetta@users.noreply.github.com>
2021-05-20 19:44:05 +0200

    add kcal to vmfpmodel

--- R/kCalibrate.R
+++ R/kCalibrate.R
@@ -1,16 +1,16 @@
-#' @title von Mises Fisher Procrustes model
-#' @description perform the functional alignment using the von Mises Fisher Procrustes model
-#' @usage kCalibrate(data)
-#' @param D squared euclidean distance matrix
-#' @param p distances to compute bandwith value
+#' @title Calibrate parameters 
+#' @description Calibrates the parameters of the von Mises-Fisher distribution to be used as prior in the von Mises-Fisher-Procrustes model.
+#' @usage kCalibrate(D, p = 0.01)
+#' @param D squared euclidean distance matrix between the coordinates of the voxels
+#' @param p distances to compute bandwith value. Default value is 0.01
 #' @author Angela Andreella and Daniela Corbetta
-#' @return Returns list of matrices
+#' @return Returns directly \code{k*Q}, i.e. the product between the concentration parameter and the location matrix parameter of the prior distribution
 #' @export
 #' @importFrom diffusionMap epsilonCompute 
 #' 
 #' 
 
-kCalibrate <- function(D, p){
+kCalibrate <- function(D, p = 0.01){
   
   eps <- epsilonCompute(D, p = p)
   #kQ <- exp(D*(1-1/eps)) %*% solve(exp(-D))
