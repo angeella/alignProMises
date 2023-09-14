b8b8f797a8c9054f89ad8d2dbb36d92912752bac
Daniela Corbetta <Corbettadaniela@gmail.com>
2023-09-14 17:29:03 +0200

    Add truncation parameter l

--- R/EfficientProMisesSubj.R
+++ R/EfficientProMisesSubj.R
@@ -1,30 +1,34 @@
 #' @title Efficient ProMises model
-#' @description Performs the functional alignment using the light version of the von Mises Fisher Procrustes model allowing for different number of voxels between matrices
-#' @usage EfficientProMises(data, maxIt=10, t =.001, k = 0, Q = NULL, 
-#' ref_ds = NULL, scaling = T, reflection= T, subj= T, coord = NULL)
-#' @param data data, i.e., list of matrices with dimension time points - voxels. Matrices can have different number of columns but must have same numbers of rows.
+#' @description Performs the functional alignment using the Efficient ProMises model model allowing for different number of voxels between matrices
+#' @usage EfficientProMisesSubj(data, maxIt=10, t =.001, k = 0, Q = NULL, 
+#' ref_ds = NULL, scaling = T, reflection= T, subj= T, coord = NULL, 
+#' singleQ = F, l = NULL)
+#' @param data data, i.e., list of matrices with dimension time points - voxels. Matrices can have different number of columns but must have the same numbers of rows.
 #' @param maxIt maximum number of iteration
 #' @param t the threshold value to be reached as the minimum relative reduction between the matrices
 #' @param k value of the concentration parameter of the prior distribution
 #' @param Q value of the location parameter of the prior distribution. It has dimension time-points x time-points, it could be not symmetric.
-#' @param ref_ds starting matrix to align
+#' @param ref_ds starting matrix to align. If \code{NULL}, it is set to the element-wise arithmetic mean of the reduced matrices
 #' @param scaling Flag to apply scaling transformation
 #' @param reflection Flag to apply reflection transformation
 #' @param subj Flag if each subject has his/her own set of voxel after voxel selection step
-#' @param coord list with 3-dim coordinates of the variables. Matrices can have different coordinates. If If the location parameter \code{Q = NULL}, then \code{coord} is used to compute it
+#' @param coord list with 3-dim coordinates of the variables. Matrices can have different coordinates. If the location parameter \code{Q = NULL}, then \code{coord} is used to compute it
+#' @param l number of singular vectors required to obtained the reduced transformation of the matrices. If \code{NULL}, it is set equal to the number of rows. 
+#' @param singleQ if \code{TRUE}, the same location parameter is assumed for the prior distribution of all the rotation parameters.
 #' @author Angela Andreella and Daniela Corbetta
-#' @return \code{EfficientProMises} returns a list with four components:
+#' @return \code{EfficientProMisesSubj} returns a list with five components:
 #' \item{\code{Xest}}{a list with the aligned matrices}
 #' \item{\code{R}}{an array with the rotation matrices}
 #' \item{\code{dist}}{a vector with length equal to the number of iterations that contains the distances between a reference matrix and the previous one}
 #' \item{\code{count}}{the number of iterations done by the algorithm}
+#' \item{\code{M}}{the element-wise mean matrix of the aligned matrices.}
 #' @export
 #' @importFrom plyr aaply 
 #' @importFrom foreach foreach
 #' @importFrom foreach %dopar%
 #' @importFrom rARPACK svds
 
-EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_ds = NULL, scaling = T, reflection= T, subj= T, coord = NULL){
+EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_ds = NULL, scaling = T, reflection= T, subj= T, coord = NULL, l = NULL, singleQ = F){
   
   if(!is.list(data)){warnings("Please insert a list of matrices with dimension time points - voxels")}
   
@@ -38,14 +42,16 @@ EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_
   
   j <- row
   
+  if(is.null(l))
+    l <- row
+  
   V <- foreach(i = c(1:nsubj)) %dopar% {
-    out <- svds(matrix(unlist(data[[i]]), nrow = j), k = j)
+    out <- svds(matrix(unlist(data[[i]]), nrow = j), k = l)
     out$v 
     
   }
   # preparo l'array che contiene le X*
-  Xstar <- array(NA, dim=c(j,j,nsubj))
-  dim(Xstar)
+  Xstar <- array(NA, dim=c(j,l,nsubj))
   # da cambiare
   for (i in 1:nsubj){
     Xstar[,,i] <- matrix(unlist(data[[i]]), nrow = row) %*% matrix(unlist(V[[i]]), nrow = col[i])
@@ -54,9 +60,9 @@ EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_
   ref_ds <- colMeans(aperm(Xstar, c(3, 1, 2)))
   
   Xest <-  array(NA, dim(Xstar))
-  R <-  array(NA, c(row, row, nsubj))
+  R <-  array(NA, c(l, l, nsubj))
   
-  # Calcolo Q*=exp(-D)
+  # Compute Q*=exp(-D)
     if(is.null(Q)){
       if(subj){
         Qstar <- foreach(i = c(1:nsubj)) %dopar% {
@@ -67,7 +73,7 @@ EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_
             exp(-as.matrix(D))
           }
           else 
-            matrix(0, nrow = row, ncol = row)
+            matrix(0, nrow = l, ncol = l)
         }
         
       }
@@ -81,7 +87,7 @@ EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_
             exp(-as.matrix(D))
           }
           else 
-            matrix(0, nrow = row, ncol = row)
+            matrix(0, nrow = l, ncol = l)
         }
       }
     }
@@ -89,17 +95,27 @@ EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_
   
   
   while(dist[count] > t & count < maxIt){
-    
-    out <- foreach(i = c(1:nsubj)) %dopar% {
-
+    # Different location parameter for each image
+    if(!singleQ){
+      out <- foreach(i = c(1:nsubj)) %dopar% {
+        
         GPASub(Xstar[,,i], Qstar[[i]], k, kQ = NULL, ref_ds, scaling, reflection, centered=F)
-      
+        
+      }
+    }
+    # else single parameter for every image
+    else{
+      out <- foreach(i = c(1:nsubj)) %dopar% {
+        
+        GPASub(Xstar[,,i], Qstar, k, kQ = NULL, ref_ds, scaling, reflection, centered=F)
+        
+      }
     }
     count <- count + 1 
     Xest = array(unlist(sapply(c(1:nsubj), function(x) out[[x]]$Xest,simplify = F)), dim = dim(Xstar))
-    R = array(unlist(sapply(c(1:nsubj), function(x) out[[x]]$R,simplify = F)), dim = c(row,row,nsubj))
+    R = array(unlist(sapply(c(1:nsubj), function(x) out[[x]]$R,simplify = F)), dim = c(l,l,nsubj))
     ref_ds_old = ref_ds
-    # ref_ds = aaply(Xest, c(1,2), mean)
+   
     ref_ds <- colMeans(aperm(Xest, c(3, 1, 2)))
     dist[count] <- norm(ref_ds-ref_ds_old, type="F")
   }
@@ -109,7 +125,7 @@ EfficientProMisesSubj <- function(data, maxIt=10, t =.001, k = 0, Q = NULL, ref_
     Xest1[[i]] <- Xest[,,i] %*% t(matrix(unlist(V[[i]]), nrow = col[i]))
   # for (i in 1:nsubj) Xest1[,,i] <- Xest[,,i] %*% t(V)
   
-  return(list(Xest = Xest1, R = R, dist = dist,  count = count))
+  return(list(Xest = Xest1, R = R, dist = dist,  count = count, M = ref_ds))
 }
 
 
